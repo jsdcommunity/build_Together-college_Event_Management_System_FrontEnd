8b4b272482814ab5c1e28deaeec088179e012774
MohamedDanis <danishmanu17@gmail.com>
2021-10-17 12:17:26 +0530

    Added folder structure and tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-            
+            <h1 className="text-black font-extrabold text-center mt-4">Hello world</h1>
         </div>
     )
 }
